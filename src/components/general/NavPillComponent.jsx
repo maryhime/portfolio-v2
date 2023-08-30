3daccecbfc1b54bee5c360c6b837f29cb6c50754
Mary Caseria <mtmc@newmediastaff.com>
2023-08-30 17:56:44 +0800

    #1 Nav pill hover fix

--- src/components/general/NavPillComponent.jsx
+++ src/components/general/NavPillComponent.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 export const NavPillComponent = ({ title, style }) => {
 
   return (
-    <button className={`p-8 flex gap-[10px] rounded-button h-button text-button uppercase w-full justify-center items-center hover:bg-primary-500hover:text-white ${style}`}>
+    <button className={`p-8 flex gap-[10px] rounded-button h-button text-button uppercase w-full justify-center items-center hover:bg-primary-500 hover:text-white ${style}`}>
       {title}
     </button>
 
