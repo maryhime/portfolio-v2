3eb41f323f7aa45330d86ff537e37ca50274ba36
Mary Caseria <mtmc@newmediastaff.com>
2023-08-31 16:00:30 +0800

    #5 added Tools Page

---
+++ src/components/general/ToolsCardComponent.jsx
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react'
+import { TOOLS } from '../../utils/data'
+import { CardContainerComponent } from './CardContainerComponent'
+
+export const ToolsCardComponent = () => {
+  return (
+    TOOLS.map((value, index) =>
+    <CardContainerComponent key={index} style={'pb-[48px]'}>
+      <div className="flex flex-col gap-48 pt-24 px-24 desktop:px-64 items-center h-full">
+          <Fragment >
+            <h3 className="text-tools-card desktop:text-title uppercase text-center">
+              {value.title}
+            </h3>
+            <div className={`grid ${value.column} align-center w-fit gap-24`}>
+              {value.icons.map((icons, index) =>
+                <img key={index} src={`icons/${icons}`} alt="" />
+              )}
+            </div>
+          </Fragment>
+
+
+       
+
+      </div>
+    </CardContainerComponent>
+    )
+  )
+}
---
+++ src/pages/ToolsPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { MainHeaderComponent } from '../components/general/MainHeaderComponent'
+import { ToolsCardComponent } from '../components/general/ToolsCardComponent'
+
+const ToolsPage = () => {
+  return (
+    <div className="flex flex-col gap-24 laptop:gap-48 laptop-lg:gap-64 desktop:gap-64 w-full">
+       <MainHeaderComponent
+        title={'Tools'}
+        subtitle={'Tools that I use to create designs, develop websites, and keep track of my studies and notes. '} 
+        style={'laptop:w-[50%]'} 
+        />
+        <div className="grid grid-cols-1 tablet:grid-cols-2 gap-24">
+          <ToolsCardComponent/>
+        </div>
+        
+
+
+    </div>
+  )
+}
+
+export default ToolsPage
