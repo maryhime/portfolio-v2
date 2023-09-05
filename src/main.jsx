5a2a92da865ea6edb29570bd3efcf2f00c7e0f97
Reno Angelo <personal.renoangelo@gmail.com>
2023-09-05 22:07:15 +0800

    refs #9 removed unused import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { PAGES } from './routes';
 import index from './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
