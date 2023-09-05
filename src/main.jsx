fba20e969edd371b583978ff9ebd722c2d0761fb
Reno Angelo <personal.renoangelo@gmail.com>
2023-09-05 21:29:22 +0800

    refs #7 react router setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { PAGES } from './routes';
 import index from './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   
-    {PAGES.map((item) => item.component)}
+    <BrowserRouter>
+      {PAGES.map((item) => item.component)}
+    </BrowserRouter>
   </React.StrictMode>
 )
 
