e80bfc07bb9a4cf450f8a6dd8a260edb0ffe6c31
Mary Therese Caseria <caseria.therese@gmail.com>
2023-09-07 21:58:58 +0800

    #3 added under construction page for case study

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 export const PageNotFound = () => {
   return (
-    <div>
-      404 PAGE NOT FOUND
+    <div className="flex flex-col gap-64 items-center">
+      <img
+        className="laptop:h-[400px]"
+        src="/images/404-illustration.svg"
+        alt=""
+      />
+      <div className="flex flex-col gap-16 text-center">
+        <h1 className="text-title uppercase">under construction</h1>
+        <p className="text-body text-body-text desktop:text-body-desktop">
+          This section is under construction and will be back after a short while.
+        </p>
+      </div>
     </div>
   );
-}
+};
