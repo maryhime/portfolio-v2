c2a965d35138199d776b4192c496ab42121a2de4
Reno Angelo <personal.renoangelo@gmail.com>
2023-09-19 03:47:07 +0800

    refs #7 removed unused imports and code

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,36 +1,33 @@
+import React from "react";
 
-import React, { lazy } from 'react';
-
-import CaseStudiesPage from './pages/CaseStudies/CaseStudiesPage.jsx';
-import IllustrationPage from './pages/Illustration/IllustrationPage.jsx';
-import ToolsPage from './pages/Tools/ToolsPage.jsx';
-import WebDesignPage from './pages/WebDesign/WebDesignPage';
-import { PageNotFound } from './components/PageNotFound.jsx';
-import ResumePage from './pages/Resume/ResumePage.jsx';
+import IllustrationPage from "./pages/Illustration/IllustrationPage.jsx";
+import ToolsPage from "./pages/Tools/ToolsPage.jsx";
+import WebDesignPage from "./pages/WebDesign/WebDesignPage";
+import { PageNotFound } from "./components/PageNotFound.jsx";
 
 export const PAGES = [
   {
-    'id': 1,
-    'name': 'Web Design',
-    'component': <WebDesignPage />,
-    'path': '/',
+    id: 1,
+    name: "Web Design",
+    component: <WebDesignPage />,
+    path: "/",
   },
   {
-    'id': 2,
-    'name': 'Studies',
-    'component': <PageNotFound/>,
-    'path': '/case-study',
+    id: 2,
+    name: "Studies",
+    component: <PageNotFound />,
+    path: "/case-study",
   },
   {
-    'id': 3,
-    'name': 'Illustrations',
-    'component': <IllustrationPage s/>,
-    'path': '/illustrations',
+    id: 3,
+    name: "Illustrations",
+    component: <IllustrationPage />,
+    path: "/illustrations",
   },
   {
-    'id': 4,
-    'name': 'Tools',
-    'component': <ToolsPage />,
-    'path': '/tools',
+    id: 4,
+    name: "Tools",
+    component: <ToolsPage />,
+    path: "/tools",
   },
 ];
