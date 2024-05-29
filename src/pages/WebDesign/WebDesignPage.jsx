984afd8f2c9f03517d9bae67804a1a7af20780ab
Mary Therese Caseria <caseria.therese@gmail.com>
2024-05-29 17:03:53 +0800

    updated resume

--- src/pages/WebDesign/WebDesignPage.jsx
+++ src/pages/WebDesign/WebDesignPage.jsx
@@ -1,25 +1,25 @@
-import React, { Fragment } from 'react'
-import { MainHeaderComponent } from '../../components/general/MainHeaderComponent'
-import { WebDesignCardComponent } from '../../components/general/WebDesignCardComponent'
-import { WEB_DESIGN } from '../../utils/data'
-import { CardContainerComponent } from '../../components/general/CardContainerComponent'
+import React, { Fragment } from "react";
+import { MainHeaderComponent } from "../../components/general/MainHeaderComponent";
+import { WebDesignCardComponent } from "../../components/general/WebDesignCardComponent";
+import { WEB_DESIGN } from "../../utils/data";
+import { CardContainerComponent } from "../../components/general/CardContainerComponent";
 
 const WebDesignPage = () => {
   return (
-    <div className="flex flex-col phone-lg:flex-row laptop:gap-48 laptop-lg:gap-64 w-full ">
-
-      <div className='flex flex-col gap-64 '>
+    <div className="phone-lg:flex-row laptop:gap-48 laptop-lg:gap-64 flex flex-col w-full">
+      <div className=" flex flex-col gap-64">
         <MainHeaderComponent
-          title={'Web Design & Development'}
-          subtitle={'Website Pages and applications that I have created during the course of my career and free time.'}
+          title={"Web Design & Development"}
+          subtitle={
+            "Website Pages and applications that I have created during the course of my career and free time."
+          }
         />
 
-
         <div className="desktop:flex laptop:flex-col laptop:gap-48 hidden">
-          {WEB_DESIGN.slice(0, 6).map((value, index) =>
+          {WEB_DESIGN.slice(0, 6).map((value, index) => (
             <CardContainerComponent
               key={index}
-              style={'hover:text-primary-500'}
+              style={"hover:text-primary-500"}
             >
               <WebDesignCardComponent
                 title={value.title}
@@ -28,13 +28,12 @@ const WebDesignPage = () => {
                 link={value.link}
               />
             </CardContainerComponent>
-
-          )}
+          ))}
         </div>
 
         {/* laptop */}
-        <div className="laptop:flex laptop:flex-col laptop:gap-48 hidden desktop:hidden">
-          {WEB_DESIGN.slice(0, 8).map((value, index) =>
+        <div className="laptop:flex laptop:flex-col laptop:gap-48 desktop:hidden hidden">
+          {WEB_DESIGN.slice(0, 8).map((value, index) => (
             <CardContainerComponent key={index}>
               <WebDesignCardComponent
                 title={value.title}
@@ -43,16 +42,16 @@ const WebDesignPage = () => {
                 link={value.link}
               />
             </CardContainerComponent>
-
-          )}
+          ))}
         </div>
 
         {/* mobile */}
-        <div className="grid grid-cols-1 tablet:grid-cols-2 gap-24 laptop:hidden">
-          {WEB_DESIGN.map((value, index) =>
+        <div className="tablet:grid-cols-2 laptop:hidden grid grid-cols-1 gap-24">
+          {WEB_DESIGN.map((value, index) => (
             <CardContainerComponent
               key={index}
-              style={'hover:text-primary-500'}>
+              style={"hover:text-primary-500"}
+            >
               <WebDesignCardComponent
                 title={value.title}
                 category={value.desc}
@@ -60,18 +59,14 @@ const WebDesignPage = () => {
                 link={value.link}
               />
             </CardContainerComponent>
-          )}
+          ))}
         </div>
-
       </div>
 
       {/* laptop */}
-      <div className=" hidden laptop:flex laptop:flex-col laptop:gap-48 desktop:hidden">
-        {WEB_DESIGN.slice(8, 16).map((value, index) =>
-          <CardContainerComponent
-            key={index}
-            style={'hover:text-primary-500'}
-          >
+      <div className=" laptop:flex laptop:flex-col laptop:gap-48 desktop:hidden hidden">
+        {WEB_DESIGN.slice(8, 16).map((value, index) => (
+          <CardContainerComponent key={index} style={"hover:text-primary-500"}>
             <WebDesignCardComponent
               title={value.title}
               category={value.desc}
@@ -79,15 +74,13 @@ const WebDesignPage = () => {
               link={value.link}
             />
           </CardContainerComponent>
-        )}
+        ))}
       </div>
 
       {/* desktop */}
-      <div className=" hidden desktop:flex desktop:flex-col desktop:gap-48">
-        {WEB_DESIGN.slice(6, 12).map((value, index) =>
-          <CardContainerComponent
-            key={index}
-            style={'hover:text-primary-500'}>
+      <div className=" desktop:flex desktop:flex-col desktop:gap-48 hidden">
+        {WEB_DESIGN.slice(6, 12).map((value, index) => (
+          <CardContainerComponent key={index} style={"hover:text-primary-500"}>
             <WebDesignCardComponent
               title={value.title}
               category={value.desc}
@@ -95,14 +88,11 @@ const WebDesignPage = () => {
               link={value.link}
             />
           </CardContainerComponent>
-        )}
+        ))}
       </div>
-      <div className=" hidden desktop:flex desktop:flex-col desktop:gap-48">
-        {WEB_DESIGN.slice(12, 16).map((value, index) =>
-          <CardContainerComponent 
-          key={index}
-          style={'hover:text-primary-500'}
-          >
+      <div className=" desktop:flex desktop:flex-col desktop:gap-48 hidden">
+        {WEB_DESIGN.slice(12, 16).map((value, index) => (
+          <CardContainerComponent key={index} style={"hover:text-primary-500"}>
             <WebDesignCardComponent
               title={value.title}
               category={value.desc}
@@ -110,12 +100,10 @@ const WebDesignPage = () => {
               link={value.link}
             />
           </CardContainerComponent>
-        )}
+        ))}
       </div>
-
     </div>
+  );
+};
 
-  )
-}
-
-export default WebDesignPage
+export default WebDesignPage;
