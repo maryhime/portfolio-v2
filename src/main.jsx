07fd2d41f108202483c45d4ab93cac1857056682
Reno Angelo D. Banderlipe <personal.renoangelo@gmail.com>
2025-08-22 19:43:00 +0800

    chore: speed insight

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import "@/index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
