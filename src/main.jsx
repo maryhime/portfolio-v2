2674a24459f8027be737a4fa3e8faa71dd744716
Reno Angelo Banderlipe <92479031+renoangelobanderlipe@users.noreply.github.com>
2025-08-22 19:39:30 +0800

    chore: add vercel analytics (#19)

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "@/index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>
 );
