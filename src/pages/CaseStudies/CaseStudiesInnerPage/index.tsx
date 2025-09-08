84be6bb7fdb229a5904eb84fdf651f817dcb71a8
Reno Angelo D. Banderlipe <personal.renoangelo@gmail.com>
2025-09-08 18:03:35 +0800

    temp

--- src/pages/CaseStudies/CaseStudiesInnerPage/index.tsx
+++ src/pages/CaseStudies/CaseStudiesInnerPage/index.tsx
@@ -1,30 +1,57 @@
 import { useParams } from "react-router-dom";
 import { CASE_STUDIES_MOCK_DATA } from "@/_mock/case-studies";
 import { InnerPagesHeader } from "@/components/template/InnerPagesHeader";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const CaseStudiesInnerPage = () => {
   const { id } = useParams();
+  const [scrollY, setScrollY] = useState(0);
+
   const caseStudies = CASE_STUDIES_MOCK_DATA.find(
     (item) => item.id === Number(id) // convert param to number
   );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   if (!caseStudies) {
     return <div>Design not found</div>;
   }
 
+  // normalize: 0 = top, 1 = scrolled 200px (cap at 1)
+  const scrollProgress = Math.min(scrollY / 200, 1);
+
+  console.log("scrollProgress", scrollProgress);
   return (
-    <div className={"flex laptop:overflow-hidden laptop:h-screen flex-col select-none bg-[#FCFCFC]"}>
-      <InnerPagesHeader
-        title={caseStudies.project_name}
-        subtitle={caseStudies.subtitle}
-        hasButton={true}
-        designGallery={false}
-        backgroundImage={caseStudies.header_bg}
-        external_link={true}
-        link={caseStudies.project_link}
-        color_theme={caseStudies.color_theme}
-        button_style={caseStudies.button_style}
-      />
+    <div
+      className={
+        "flex laptop:overflow-hidden flex-col select-none bg-[#FCFCFC]"
+      }
+      // className={
+      //   "flex laptop:overflow-hidden laptop:h-screen flex-col select-none bg-[#FCFCFC]"
+      // }
+    >
+      {scrollProgress ? (
+        "NEW SCROLL HEADER"
+      ) : (
+        <InnerPagesHeader
+          title={caseStudies.project_name}
+          subtitle={caseStudies.subtitle}
+          hasButton={true}
+          designGallery={false}
+          backgroundImage={caseStudies.header_bg}
+          external_link={true}
+          link={caseStudies.project_link}
+          color_theme={caseStudies.color_theme}
+          button_style={caseStudies.button_style}
+        />
+      )}
 
       {/* content */}
       <div className="overflow-auto flex flex-col w-full">
